0964a91a979ad0998ed23d38158c0cbcf3bf00f0
OLEG <schastliviy.sluchay@ukr.net>
2022-04-02 15:31:55 +0300

    Favicon_and_page_title (added favicon, title and fix bug with React DOM.render)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 import "./index.css";
-import App from './App';
+import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
-
-
