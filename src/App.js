abf4a2625920013a8e51bcd310df4ba2eb914438
Taras Demchenko <80649931+Meelenych@users.noreply.github.com>
2022-04-03 12:58:58 +0300

    Private and Public routes logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
 import { Container } from "./styles/Container";
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "./styles/GlobalStyles";
+// import { useSelector } from "react-redux";
+// import PrivateRoute from "./components/Routes/PrivateRoute";
+// import PublicRoute from "./components/Routes/PublicRoute";
+// import authSelectors from "./store/auth/auth-selectors";
 
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
@@ -12,20 +15,25 @@ const StatisticsPage = lazy(() =>
 );
 
 function App() {
+	// const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
 	return (
+		// !isFetchingCurrentUser && (
 		<>
 			<GlobalStyles />
-
 			<Suspense fallback={<p>Loading...</p>}>
-				<Routes>
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/" element={<MainPage />} />
-					<Route path="/statistics" element={<StatisticsPage />} />
-				</Routes>
+				<Container>
+					<Routes>
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/register" element={<RegisterPage />} />
+						<Route path="/" element={<MainPage />} />
+						<Route path="/statistics" element={<StatisticsPage />} />
+					</Routes>
+				</Container>
 			</Suspense>
 		</>
 	);
+	// );
 }
 
 export default App;
---
+++ src/store/auth/auth-selectors.js
@@ -0,0 +1,13 @@
+const getIsLoggedIn = (state) => state.auth.isLoggedIn;
+const getUsername = (state) => state.auth.user.name;
+const getUserEmail = (state) => state.auth.user.email;
+const getIsFetchingCurrent = (state) => state.auth.isFetchingCurrentUser;
+
+const authSelectors = {
+	getIsLoggedIn,
+	getUsername,
+	getUserEmail,
+	getIsFetchingCurrent,
+};
+
+export default authSelectors;
