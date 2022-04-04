792ccdcae13120a9c7238eaa30a157b0e55f1178
EvgeniyaBarabash <barabash1989@gmail.com>
2022-04-04 23:51:40 +0300

    mainBg

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,13 @@
+import MainBg  from "../../components/MainBg/MainBg";
 import Navigation from "../../components/Navigation/Navigation";
 
 export default function MainPage() {
 	return (
 		<>
 			<Navigation />
+			<MainBg>
 			<h1>MainPage</h1>
+			</MainBg>
 		</>
 	);
 }
