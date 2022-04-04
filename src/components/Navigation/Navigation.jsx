1230c1f132908b13695c827ca3ad908837f1c85b
Taras Demchenko <80649931+Meelenych@users.noreply.github.com>
2022-04-05 00:44:39 +0300

    Links text styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 export default function Navigation() {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<NavLink to="/">Main</NavLink>
-				</li>
-				<li>
-					<NavLink to="/statistics">Statistics</NavLink>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? styles.linkStyleActive : styles.linkStyle
+            }
+          >
+            Главная
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/statistics"
+            className={({ isActive }) =>
+              isActive ? styles.linkStyleActive : styles.linkStyle
+            }
+          >
+            Статистика
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
