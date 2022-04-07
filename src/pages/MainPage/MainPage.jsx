9682d515a1606f2dab499bd303eb3b18ef67354c
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-04-07 21:58:54 +0200

    delete empty files

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,19 +1,19 @@
-import MainBg  from "../../components/MainBg/MainBg";
-import Navigation from "../../components/Navigation/Navigation";
-import Header from "./components/Header/Header";
+import MainBg from '../../components/MainBg/MainBg';
+import Navigation from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
 
 import { Container } from '../../styles/Container';
 
 export default function MainPage() {
-	return (
-		<>
-			<Navigation />
-    <Header/>
+  return (
+    <>
+      <Header />
+      <Navigation />
       <MainBg>
-     <Container>
-			<h1>MainPage</h1>
-      </Container>
-			</MainBg>
-		</>
-	);
+        <Container>
+          <h1>MainPage</h1>
+        </Container>
+      </MainBg>
+    </>
+  );
 }
