7f366a9de0f913e343b612cbf32fe4345c625da5
EvgeniyaBarabash <barabash1989@gmail.com>
2022-04-08 01:03:18 +0300

    2

--- src/store/rootReducer.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from 'redux';
-// import { modalReducer } from './modal/modal'
-// const rootReducer = combineReducers({
-//     modal: modalReducer,
-// });
-
-// export default rootReducer;
