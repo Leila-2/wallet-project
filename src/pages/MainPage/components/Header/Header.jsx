ca3f1f4e8ef6805bfc938aa467ad6750b2901c6e
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2022-04-07 23:06:38 +0800

    header/correction of remarks

--- src/pages/MainPage/components/Header/Header.jsx
+++ src/pages/MainPage/components/Header/Header.jsx
@@ -1,37 +1,26 @@
 import React from 'react';
-import { ReactComponent as Logo } from './icons/Group.svg';
-import { ReactComponent as Exit } from './icons/exit 1.svg';
-import { ReactComponent as Slash } from './icons/Vector 4.svg';
+import Media from "react-media";
 import StyledHeader from './StyledHeader';
+import { baseTheme } from '../../../../styles/variables';
 import { Container } from '../../../../styles/Container';
-import { Link } from 'react-router-dom';
+import { renderMobile, render } from './MediaComponents';
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Container className="Container">
-        <Link to="/" className="FlexGroops">
-          <Logo className="Logo" />
-          <h1 className="Title">Wallet</h1>
-        </Link>
-        <div className="FlexGroops FontButton">
-          <div className="Avatar"></div>
-          <button
-            className="HeaderButton Name"
-            onClick={() => console.log('name')}
-          >
-            Имя
-          </button>
-          <Slash className="Slash Hidden" />
-          <button
-            className="HeaderButton ExitIcon FlexGroops"
-            onClick={() => console.log('exit')}
-          >
-            <Exit />
-            <p className="Exit Hidden">Выход</p>
-          </button>
-        </div>
-      </Container>
+       <Media
+        queries={{
+          small: baseTheme.media.mobileMax,
+          medium: `${baseTheme.media.tabletMin || baseTheme.media.desktop}`,
+        }}
+      >
+        {matches => (
+          <Container>
+            {matches.small && renderMobile()}
+            {matches.medium && render()}
+          </Container>
+        )}
+        </Media>
     </StyledHeader>
   );
 };
