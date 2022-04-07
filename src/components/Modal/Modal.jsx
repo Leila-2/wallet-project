752f954f9407251476a3207ff31b34fe8d8c01e5
EvgeniyaBarabash <barabash1989@gmail.com>
2022-04-08 00:58:21 +0300

    2commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,8 @@
 import { useEffect,useState } from 'react';
 import { createPortal } from 'react-dom';
-import{ ModalWrapper} from './Modal.styled';
+import{ ModalWrapper, Checkbox,СancelBtn} from './Modal.styled';
 import {ReactComponent as AddIconClose} from './closeBtn.svg'
+import FormButton from '../FormButton/FormButton';
 const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ showModal,setShowModal,children}) {
@@ -28,7 +29,46 @@ const onEscapeClose=e=>{
   return createPortal(
     <ModalWrapper onClick={handleClickBackdrop}>
       <div className="content"><AddIconClose className="closeBtn" type='button' onClick={onClose}/>
-      {children}
+      <p className="title">Добавить транзакцию</p>
+      <form className='form'>
+<Checkbox>
+          <span className="checkbox__label-right">Доход</span>
+          <span className="checkbox__toggle">
+            <input
+              name='type'
+              type='checkbox'
+              className="checkboxInput"
+              id='checkbox'
+            />
+            <label htmlFor='checkbox'></label>
+          </span>
+          <span className="checkbox__label-left">Расход</span>
+        </Checkbox>
+        <select
+          className="select"
+          placeholder='Выберите категорию'
+        />
+        <div className='inputCont'>
+         <input
+          className="numberInput"
+          type='number'
+          placeholder='0.00'
+          required
+        />
+        <input
+          className="dateInput"
+          type='date'
+          required
+        />
+        </div>
+         <textarea
+          className="comment"
+          name='comment'
+          placeholder='Комментарий'
+        />
+        <FormButton title={"Добавить"}/>
+        <СancelBtn>Отмена</СancelBtn>
+      </form>
       </div>
     </ModalWrapper>,
     modalRoot,
