e92ccd95ab4995bc2553715db40b32d8de752d6b
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-04-08 15:43:08 +0200

    hot fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+//import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
