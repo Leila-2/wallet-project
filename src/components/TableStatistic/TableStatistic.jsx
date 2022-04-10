3f7d7c2b43b71c8af1e40c4a8317151f9473306c
Marina-93 <marina.ua93@gmail.com>
2022-04-10 22:44:20 +0300

    TableStatusFix

--- src/components/TableStatistic/TableStatistic.jsx
+++ src/components/TableStatistic/TableStatistic.jsx
@@ -4,59 +4,82 @@ export default function TableStatistic() {
   return (
     <StyledTableStatistic>
       <table className="global-table">
-        <thead className="header-table">
-          <tr>
+          <tr className="header-table">
             <th className="first-table">Категория</th>
             <th className="last-table">Сумма</th>
           </tr>
-        </thead>
 
-        <tbody>
           <tr className="table-list">
-            <td className='table-categori'>Основные расходы</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Основные расходы</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Продукты</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Продукты</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Машина</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Машина</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Забота о себе</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Забота о себе</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Забота о детях</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Забота о детях</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Товары для дома</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Товары для дома</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Образование</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Образование</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Досуг</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Досуг</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <td className='table-categori'>Другие расходы</td>
-            <td className='table-data'>5000</td>
+            <td className="table-categori">
+              <div className="square"></div>
+              <span className="text">Другие расходы</span>
+            </td>
+            <td className="table-data">5000</td>
           </tr>
           <tr className="table-list">
-            <th className='table-title'>Расходы:</th>
-            <td className='table-data'>5000</td>
+            <th className="table-title">Расходы:</th>
+            <td className="last-data">5000</td>
           </tr>
           <tr className="table-list">
-            <th className='table-title'>Доходы:</th>
-            <td className='table-data'>5000</td>
+            <th className="table-title">Доходы:</th>
+            <td className="last-data">5000</td>
           </tr>
-        </tbody>
       </table>
     </StyledTableStatistic>
   );
