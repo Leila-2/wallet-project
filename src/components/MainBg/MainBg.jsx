6290e68af3622076e8f8fe457f07caf55fb3fb8c
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-04-10 12:28:17 +0200

    add mainBg with container

--- src/components/MainBg/MainBg.jsx
+++ src/components/MainBg/MainBg.jsx
@@ -1,11 +1,16 @@
 import { MainBgImage } from './MainBg.styled.jsx';
+import { Container } from '../../styles/Container';
 
 export default function MainBg({ RigthComponent, LeftComponent }) {
   return (
     <>
       <MainBgImage>
-        <div className="wrapper-right">{RigthComponent}</div>
-        <div>{LeftComponent}</div>
+        <div className="wrapper-right">
+          <Container>{RigthComponent}</Container>
+        </div>
+        <div>
+          <Container>{LeftComponent}</Container>
+        </div>
       </MainBgImage>
     </>
   );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import ButtonAddTransaction from '../../components/BtnAddTransaction/BtnAddTransaction';
 import Modal from '../../components/Modal/Modal';
 import { useEffect, useState } from 'react';
-import { Container } from '../../styles/Container';
+//import { Container } from '../../styles/Container';
 
 export default function MainPage() {
   const [showModal, setShowModal] = useState(false);
@@ -14,13 +14,10 @@ export default function MainPage() {
   return (
     <>
       <Header />
-      <Container>
-        <MainBg RigthComponent={<Navigation />} />
-        <ButtonAddTransaction onClick={onClose} />
-        {showModal && (
-          <Modal showModal={showModal} setShowModal={setShowModal} />
-        )}
-      </Container>
+
+      <MainBg RigthComponent={<Navigation />} />
+      <ButtonAddTransaction onClick={onClose} />
+      {showModal && <Modal showModal={showModal} setShowModal={setShowModal} />}
     </>
   );
 }
