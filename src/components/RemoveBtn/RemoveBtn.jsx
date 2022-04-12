39201d2a6f7fecbcf92cab5cd48a25f5ef5365a6
Taras Demchenko <80649931+Meelenych@users.noreply.github.com>
2022-04-13 00:51:38 +0300

    basic components

---
+++ src/components/RemoveBtn/RemoveBtn.jsx
@@ -0,0 +1,10 @@
+import TrashSvg from './TrashSvg';
+
+const RemoveBtn = () => {
+  return (
+    <button>
+      <TrashSvg />
+    </button>
+  );
+};
+export default RemoveBtn;
---
+++ src/components/RemoveBtn/TrashSvg.jsx
@@ -0,0 +1,6 @@
+import { ReactComponent as Trash } from './trash.svg';
+
+const TrashSvg = () => {
+  return <Trash />;
+};
+export default TrashSvg;
