0c29cea48ba95f6029f437289eff0918bc9d0a95
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-04-13 15:28:28 +0200

    fix

--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,6 +1,10 @@
 import Navigation from '../../pages/MainPage/components/Navigation/Navigation';
 import Header from '../../pages/MainPage/components/Header/Header';
+import Media from 'react-media';
+import MobileBg from '../../components/MobBg/MobBg';
 
+import { Fragment } from 'react';
+import { Container } from '../../styles/Container';
 import Chart from '../../components/Chart';
 import MainBg from '../../components/MainBg/MainBg';
 import Modal from '../../components/Modal/Modal';
@@ -9,6 +13,8 @@ import { useState, useEffect } from 'react';
 import Balance from '../../components/Balance/Balance';
 import transactionOperations from '../../store/transactions/transaction-operations';
 import { useDispatch, useSelector } from 'react-redux';
+import Currency from '../../components/Currency/Currency';
+//import TableBalans from '../../components/TableBalans/Table/TableBalans';
 
 import Period from '../../components/Period';
 
@@ -37,28 +43,89 @@ export default function StatisticsPage() {
 
   return (
     <>
-      <Header />
-
-      <MainBg
-        RigthComponent={
-          <>
-            <Navigation /> <Balance />
-          </>
-        }
-        LeftComponent={
-          <>
-            <Chart />
-            <div>
-              <Period
-                setRequestedMonth={setMonth}
-                setRequestedYear={setYear}
-                years={years}
-              />
-              <TableStatistic />
-            </div>
-          </>
-        }
-      ></MainBg>
+      <div>
+        <Media
+          queries={{
+            small: '(max-width: 599px)',
+            medium: '(min-width: 600px) and (max-width: 1199px)',
+            large: '(min-width: 1200px)',
+          }}
+        >
+          {matches => (
+            <Fragment>
+              {matches.small && (
+                <>
+                  <Header />
+                  <MobileBg>
+                    <Container>
+                      <Navigation />
+                      <Chart />
+                      <Period
+                        setRequestedMonth={setMonth}
+                        setRequestedYear={setYear}
+                        years={years}
+                      />
+                      <TableStatistic />
+                    </Container>
+                  </MobileBg>
+                </>
+              )}
+              {matches.medium && (
+                <>
+                  <Header />
+                  <MainBg
+                    RigthComponent={
+                      <>
+                        <Navigation />
+                        <Balance />
+                        <Chart />
+                      </>
+                    }
+                    LeftComponent={
+                      <>
+                        <Currency />
+                        <Period
+                          setRequestedMonth={setMonth}
+                          setRequestedYear={setYear}
+                          years={years}
+                        />
+                        <TableStatistic />
+                      </>
+                    }
+                  />
+                </>
+              )}
+              {matches.large && (
+                <>
+                  <Header />
+                  <MainBg
+                    RigthComponent={
+                      <>
+                        <Navigation />
+                        <Balance />
+                        <Currency />
+                      </>
+                    }
+                    LeftComponent={
+                      <>
+                        <Chart />
+                        <div>
+                          <Period
+                            setRequestedMonth={setMonth}
+                            setRequestedYear={setYear}
+                            years={years}
+                          />
+                          <TableStatistic />
+                        </div>
+                      </>
+                    }
+                  />
+                </>
+              )}
+            </Fragment>
+          )}
+        </Media>
+      </div>
 
       <ButtonAddTransaction onClick={onClose} />
       {showModal && <Modal showModal={showModal} setShowModal={setShowModal} />}
