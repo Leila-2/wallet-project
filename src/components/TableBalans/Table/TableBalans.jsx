1799a063c06da2580f07b3493719fe066725c4fd
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-04-14 14:49:56 +0200

    delete btns in table

--- src/components/TableBalans/Table/TableBalans.jsx
+++ src/components/TableBalans/Table/TableBalans.jsx
@@ -20,9 +20,9 @@ export default function TableBalans({ transactions }) {
           <tr>
             <th className="first-table">Дата</th>
             <th>Тип</th>
-            <th className='table-title'>Категория</th>
-            <th className='table-title'>Комментарий</th>
-            <th className='sum-title'>Сумма</th>
+            <th className="table-title">Категория</th>
+            <th className="table-title">Комментарий</th>
+            <th className="sum-title">Сумма</th>
             <th className="last-table">Баланс</th>
           </tr>
         </thead>
@@ -32,16 +32,24 @@ export default function TableBalans({ transactions }) {
             <tr className="table-list" key={transaction.id}>
               <td>{transaction.date}</td>
 
-              <td className='title-type'>{transaction.type === 'expenses' ? '-' : '+'}</td>
-              <td className='table-title'>{categories[transaction.category]}</td>
-              <td className='table-title'>{transaction.comment}</td>
-              <td style={{color: `${transaction.type === 'expenses' ? '#FF6596':'#24CCA7'}` }}>{transaction.amount}</td>
-      
-              <td>
-                {transaction.balance} <EditBtn />{' '}
-                <RemoveBtn id={transaction.id} />
+              <td className="title-type">
+                {transaction.type === 'expenses' ? '-' : '+'}
+              </td>
+              <td className="table-title">
+                {categories[transaction.category]}
+              </td>
+              <td className="table-title">{transaction.comment}</td>
+              <td
+                style={{
+                  color: `${
+                    transaction.type === 'expenses' ? '#FF6596' : '#24CCA7'
+                  }`,
+                }}
+              >
+                {transaction.amount}
               </td>
 
+              <td>{transaction.balance}</td>
             </tr>
           ))}
         </tbody>
